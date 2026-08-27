2e5b8e335f240e7d3da2ffc1462b3aed4de7a9f9
Jucelio Farias Coelho <juceliocoelho2022@gmail.com>
2026-08-27 13:47:11 -0300

    feat: add least-privilege GitHub Actions OIDC role

---
+++ infrastructure/terraform/modules/github-oidc/main.tf
@@ -0,0 +1,122 @@
+data "aws_caller_identity" "current" {}
+data "aws_partition" "current" {}
+
+locals {
+  github_subject = "repo:${var.github_repository}:ref:refs/heads/${var.github_branch}"
+
+  oidc_provider_arn = var.create_oidc_provider ? aws_iam_openid_connect_provider.github[0].arn : var.existing_oidc_provider_arn
+
+  ecs_cluster_arn = "arn:${data.aws_partition.current.partition}:ecs:${var.aws_region}:${data.aws_caller_identity.current.account_id}:cluster/${var.ecs_cluster_name}"
+
+  ecs_service_arns = [
+    for service_name in var.ecs_service_names :
+    "arn:${data.aws_partition.current.partition}:ecs:${var.aws_region}:${data.aws_caller_identity.current.account_id}:service/${var.ecs_cluster_name}/${service_name}"
+  ]
+}
+
+resource "aws_iam_openid_connect_provider" "github" {
+  count = var.create_oidc_provider ? 1 : 0
+
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com"
+  ]
+
+  tags = merge(var.tags, {
+    Name = "${var.name_prefix}-github-oidc"
+  })
+}
+
+resource "aws_iam_role" "github_actions_deploy" {
+  name = "${var.name_prefix}-github-actions-deploy"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = local.oidc_provider_arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+            "token.actions.githubusercontent.com:sub" = local.github_subject
+          }
+        }
+      }
+    ]
+  })
+
+  tags = merge(var.tags, {
+    Name = "${var.name_prefix}-github-actions-deploy"
+  })
+}
+
+resource "aws_iam_role_policy" "deploy" {
+  name = "${var.name_prefix}-github-actions-deploy"
+  role = aws_iam_role.github_actions_deploy.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid      = "EcrAuthorization"
+        Effect   = "Allow"
+        Action   = ["ecr:GetAuthorizationToken"]
+        Resource = "*"
+      },
+      {
+        Sid    = "PushApplicationImages"
+        Effect = "Allow"
+        Action = [
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:BatchGetImage",
+          "ecr:CompleteLayerUpload",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:InitiateLayerUpload",
+          "ecr:PutImage",
+          "ecr:UploadLayerPart"
+        ]
+        Resource = sort(tolist(var.ecr_repository_arns))
+      },
+      {
+        Sid    = "ReadAndDeployEcsServices"
+        Effect = "Allow"
+        Action = [
+          "ecs:DescribeServices",
+          "ecs:UpdateService"
+        ]
+        Resource = local.ecs_service_arns
+      },
+      {
+        Sid      = "DescribeCluster"
+        Effect   = "Allow"
+        Action   = ["ecs:DescribeClusters"]
+        Resource = local.ecs_cluster_arn
+      },
+      {
+        Sid    = "ManageTaskDefinitionRevisions"
+        Effect = "Allow"
+        Action = [
+          "ecs:DescribeTaskDefinition",
+          "ecs:RegisterTaskDefinition"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid      = "PassOnlyCloudTaskEcsRoles"
+        Effect   = "Allow"
+        Action   = ["iam:PassRole"]
+        Resource = sort(tolist(var.ecs_task_role_arns))
+        Condition = {
+          StringEquals = {
+            "iam:PassedToService" = "ecs-tasks.amazonaws.com"
+          }
+        }
+      }
+    ]
+  })
+}
