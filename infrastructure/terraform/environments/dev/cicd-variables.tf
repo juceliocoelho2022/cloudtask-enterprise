f8cda3ad0619d3879f9425553c023eae9a97f77e
Jucelio Farias Coelho <juceliocoelho2022@gmail.com>
2026-08-27 19:10:04 -0300

    fix: configure immutable GitHub OIDC identifiers

--- infrastructure/terraform/environments/dev/cicd-variables.tf
+++ infrastructure/terraform/environments/dev/cicd-variables.tf
@@ -4,6 +4,18 @@ variable "github_repository" {
   default     = "juceliocoelho2022/cloudtask-enterprise"
 }
 
+variable "github_owner_id" {
+  description = "ID imutável do owner do repositório GitHub usado no subject OIDC."
+  type        = string
+  default     = "104524218"
+}
+
+variable "github_repository_id" {
+  description = "ID imutável do repositório GitHub usado no subject OIDC."
+  type        = string
+  default     = "1346717256"
+}
+
 variable "github_deploy_branch" {
   description = "Branch autorizada a realizar deploy no ambiente dev."
   type        = string
