f0a8675290ed2525a0fc36712c5e1c1817cfd51d
Jucelio Farias Coelho <juceliocoelho2022@gmail.com>
2026-08-27 12:52:38 -0300

    feat: add random provider for runtime secrets

--- infrastructure/terraform/environments/dev/versions.tf
+++ infrastructure/terraform/environments/dev/versions.tf
@@ -6,5 +6,10 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 5.0, < 7.0"
     }
+
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.6, < 4.0"
+    }
   }
 }
